00f57625fa7085b41ebcab4f934785dc1d00c0ab
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-07-19 09:56:23 +1000

    Update atomic.sh

--- atomic.sh
+++ atomic.sh
@@ -7,16 +7,3 @@ cd /home/adminuser
 sudo apt-get update -y
 sudo apt upgrade -y
 sudo apt autoremove --assume-yes
-
-# Download the Microsoft repository GPG keys
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
-
-# Register the Microsoft repository GPG keys
-sudo dpkg -i packages-microsoft-prod.deb
-
-# Update the list of packages after we added packages.microsoft.com
-sudo apt-get update -y
-
-# Install PowerShell
-sudo apt-get install -y powershell
-
