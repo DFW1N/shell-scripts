a6434e6803c7aa0b5a98b856e0f0db5876c5d31d
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-07-19 09:05:00 +1000

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,28 @@
-#! /bin/bash
+#!/bin/bash
+# This file should be sourced
 
-sudo apt update -y
-sudo apt install python-pip -y
-sudo pip install ansible -y
-sudo ansible-galaxy install azure.azure_preview_modules
-sudo pip install -r ~/.ansible/roles/azure.azure_preview_modules/files/requirements-azure.txt
+# Change directory to user home
+cd /home/useradmin
+
+# Upgrade all packages that have available updates and remove old ones.
+sudo apt-get update
+sudo apt upgrade -y
+sudo apt autoremove --assume-yes
+
+# Install git
+sudo apt install git --assume-yes
+
+# Install azcli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Install venv and pip
+sudo apt install python3-venv --assume-yes
+sudo apt install python3-pip --assume-yes
+
+# Setup virtual environment and push home folder ownership
+sudo python3 -m venv venv
+sudo chown adminuser /home/adminuser --recursive
+
+# Install ansible and azure modules into virtual environment
+pip3 install -r https://raw.githubusercontent.com/DFW1N/shell-scripts/main/ansible-requirements.txt
+pip3 install -r https://raw.githubusercontent.com/DFW1N/shell-scripts/main/azure-requirements.txt
