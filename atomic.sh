9a5170d5314dd426c74b32e55cca36769494957e
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-07-19 09:36:26 +1000

    Update atomic.sh

--- atomic.sh
+++ atomic.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Change directory to user home
+cd /home/adminuser
+
 # Update the list of packages
 sudo apt-get update -y
+sudo apt upgrade -y
+sudo apt autoremove --assume-yes
 
 # Download the Microsoft repository GPG keys
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
