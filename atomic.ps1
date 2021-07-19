4be36246c03808988963a5079daff685bbc9525f
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-07-19 12:09:02 +1000

    Update atomic.ps1

--- atomic.ps1
+++ atomic.ps1
@@ -1,6 +1,5 @@
 #! /usr/bin/pwsh
 
 IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
-Install-AtomicRedTeam -getAtomics;
-cd /home/adminuser/AtomicRedTeam/atomics/;
+Install-AtomicRedTeam -getAtomics -InstallPath "~\AtomicRedTeam" -Force
 Invoke-AtomicTest
