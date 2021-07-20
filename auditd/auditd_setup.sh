411c4133560bc9caefe7455371543e56719c5fc6
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-07-20 12:28:35 +1000

    Update auditd_setup.sh

--- auditd/auditd_setup.sh
+++ auditd/auditd_setup.sh
@@ -20,7 +20,7 @@ curl https://raw.githubusercontent.com/DFW1N/shell-scripts/main/auditd/systemd-u
 curl https://raw.githubusercontent.com/DFW1N/shell-scripts/main/auditd/sshd > /etc/pam.d/sshd
 
 # Download Linux OMS Agent (Change /home/adminuser to your linux {Username})
-wget -P ~/home/adminuser/ https://github.com/microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.13.35-0/omsagent-1.13.35-0.universal.x64.sh
+wget -P ~/ https://github.com/microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.13.35-0/omsagent-1.13.35-0.universal.x64.sh
 # Install OMS Agent (Change -w Workspace and -s Shared key) (Log Analytics Workspaces > Workspace > Agents management > Workspace ID {VALUE} > Primary Key {VALUE})
 sudo sh ./omsagent-*.universal.x64.sh --upgrade -w {WORKSPACE ID} -s {LINUX PRIMARY KEY}
 # Copy the omsagent .conf file to the omsagent workspace
